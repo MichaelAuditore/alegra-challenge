362e8d7d74f978f00ca87132d76e34ca27feb574
michaelAuditore <michael_v613@hotmail.com>
2025-02-14 11:54:45 -0500

    feat: adjust gh action and remove logs

--- inventory-app/routes/kitchen/index.js
+++ inventory-app/routes/kitchen/index.js
@@ -7,8 +7,6 @@ export default async function (fastify, opts) {
             try {
                 await handleKitchenRequest(fastify, message);
 
-                console.log("Message Received in Socket");
-
                 socket.send(JSON.stringify({ message: "Solicitud procesada", status: true }));
             } catch (error) {
                 fastify.log.error(`Error al procesar la solicitud de productos: ${error}`);
